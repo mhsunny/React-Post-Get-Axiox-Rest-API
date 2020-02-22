a73b34606d8c3cbe631212121c856a9425decbbb
MH Sunny <sunny@oraiko.com>
2020-02-22 16:19:48 -0500

    first commit

---
+++ src/Your.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class myComponent extends Component {
+    render() {
+        return (
+            <div>
+
+
+            </div>
+        );
+    }
+}
+
+export default myComponent;
